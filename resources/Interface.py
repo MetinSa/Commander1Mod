67293165ed41d67e38590116884c87ed341af774
Metin San <metinisan@gmail.com>
2019-09-10 13:24:55 +0200

    run tests

--- resources/Interface.py
+++ resources/Interface.py
@@ -316,7 +316,7 @@ class Interface(object):
             with open(f'{self.run_path}/{chain_dir}/commander_runs.txt', 'a') as f:
                 f.write(description)
             bash_command = './runcommander.sh'
-            subprocess.call(['basu', '-c', bash_command])
+            subprocess.call(['bash', '-c', bash_command])
             # subprocess.run('export OMP_NUM_THREADS=1', shell=True)
             # subprocess.run(f'mpirun -n {n_processors} {commander1_path}/commander {self.savefile} 2>&1 | tee {chain_dir}/slurm.txt', shell=True)
             sys.exit()
