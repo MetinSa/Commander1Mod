f688faf9d4529599097b9e8a39cd2c2450d83a3f
Metin San <metinisan@gmail.com>
2019-09-10 13:18:35 +0200

    run tests

--- resources/Interface.py
+++ resources/Interface.py
@@ -315,7 +315,7 @@ class Interface(object):
             curses.endwin()
             with open(f'{self.run_path}/{chain_dir}/commander_runs.txt', 'a') as f:
                 f.write(description)
-            subprocess.run(f'./runcommander.sh {self.savefile}', shell=True)
+            subprocess.call(f'./runcommander.sh {self.savefile}', shell=True)
             # subprocess.run('export OMP_NUM_THREADS=1', shell=True)
             # subprocess.run(f'mpirun -n {n_processors} {commander1_path}/commander {self.savefile} 2>&1 | tee {chain_dir}/slurm.txt', shell=True)
             sys.exit()
