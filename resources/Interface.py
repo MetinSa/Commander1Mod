f574fb781ee451042c30890a92dc4751d2ca3942
Metin San <metinisan@gmail.com>
2019-09-11 09:30:45 +0200

    work on run method

--- resources/Interface.py
+++ resources/Interface.py
@@ -315,6 +315,8 @@ class Interface(object):
             with open(f'{self.run_path}/{chain_dir}/commander_runs.txt', 'a') as f:
                 f.write(description)
             # subprocess.run('export OMP_NUM_THREADS=1', shell=True)
+            os.system('pwd')
+            sys.exit()
             os.system(f'export OMP_NUM_THREADS=1 && mpirun -n {n_processors} {commander1_path}/commander {self.savefile} 2>&1 | tee {chain_dir}/slurm.txt')
             sys.exit()
 
