d095e09600e451595cf1f8e7543c4541c786dcaf
Metin San <metinisan@gmail.com>
2019-09-11 09:32:22 +0200

    work on run method

--- resources/Interface.py
+++ resources/Interface.py
@@ -311,14 +311,12 @@ class Interface(object):
                                           max_str_len).decode(encoding="utf-8")
         self.menu_win.refresh()
         if description:
+            os.chdir(self.run_path)
             curses.endwin()
             with open(f'{self.run_path}/{chain_dir}/commander_runs.txt', 'a') as f:
                 f.write(description)
-            # subprocess.run('export OMP_NUM_THREADS=1', shell=True)
-            os.system('pwd')
+            os.system(f'export OMP_NUM_THREADS=1 && mpirun -n {n_processors} {commander1_path}/commander {self.savefile} 2>&1 | tee {chain_dir}/slurm.txt')
             sys.exit()
-            # os.system(f'export OMP_NUM_THREADS=1 && mpirun -n {n_processors} {commander1_path}/commander {self.savefile} 2>&1 | tee {chain_dir}/slurm.txt')
-            # sys.exit()
 
     def display_module_info(self):
         """Displays author name and patch date."""
